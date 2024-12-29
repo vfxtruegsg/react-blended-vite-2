bb2603c2ae8e67a1faad3b9ce544beaf4dc85c2d
1ster14ka <danyazybastik@gmail.com>
2024-12-29 11:04:28 +0200

    add step 3

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,6 +1,18 @@
+import { useState } from 'react';
 import Text from '../components/Text/Text';
+import { nanoid } from 'nanoid';
 
 const Todos = () => {
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(todoText) {
+    setTodos(prev => [{ ...prev, text: todoText, id: nanoid() }]);
+  }
+
+  function deleteTodo(id) {
+    setTodos(prev => prev.filter(item => item.id !== id));
+  }
+
   return <Text textAlign="center">There are no any todos ...</Text>;
 };
 
